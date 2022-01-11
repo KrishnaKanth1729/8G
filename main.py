a2d3ff61804d372b5aa5cb3cccf451d1060f7697
KrishnaKanth1729 <rkrishnakanth1729@gmail.com>
2022-01-11 18:58:08 +0530

    Public

--- main.py
+++ main.py
@@ -1,19 +1,46 @@
 import discord
+import sqlite3
+import secrets
 
+conn = sqlite3.connect("tags.db")
+cursor = conn.cursor()
 client = discord.Client()
+
+cursor.execute("CREATE TABLE IF NOT EXISTS tags(id integer PRIMARY KEY AUTOINCREMENT, name text, content text, author text)")
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
 @client.event
-async def on_message(message):
+async def on_message(message: discord.Message):
     if message.author == client.user:
         return
 
     if message.content.startswith('g!'):
         if message.content == 'g!hello':
-            await message.channel.send(f"Hello {message.author.mention}")
+            await message.channel.send(f"Hello {message.author.mention}", reference=message)
     
+        elif message.content.startswith('g!create'):
+            items = message.content.split()
+            name = items[1]
+            content = " ".join(items[2:])
+            author = str(message.author).replace('#', '_')
+            print(name, content, author)
+            cursor.execute(f"SELECT * FROM tags WHERE name=?", (name,))
+            rows = cursor.fetchall()
+            print(rows)
+            if len(rows):
+                return await message.channel.send("Tag with that name already exists", reference=message)
+            cursor.execute('INSERT INTO tags(name, content, author) VALUES(?, ?, ?)', (name, content, author))
+           
+            conn.commit()
+            await message.channel.send(f"Tag successfully created", reference=message)
 
-client.run('OTI5OTU1NDUxOTA5NTc4ODEz.Ydu2gQ.ubY63JHK1Z1FXH2n4Z2yr5RL0cQ')
+        elif message.content.startswith('g!tag'):
+            items = message.content.split()
+            name = items[1]
+            cursor.execute(f"SELECT * FROM tags WHERE name=?", (name,))
+            rows = cursor.fetchall()
+            print(rows)
+client.run(secrets.TOKEN)
